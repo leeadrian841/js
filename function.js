94dc1f218c5d2c1291fd9b46e571b4a887a3cf27
leeadrian841 <leeadrian841@gmail.com>
2016-09-24 13:19:27 +0800

    long3

--- function.js
+++ function.js
@@ -1,8 +1,18 @@
 // long1
+function lengths(){
 
+}
 // long2
 
+
 // long3
+function wordReverse(str){
+  var reversestr = str.split(" ").reverse().join(" ")
+  return reversestr
+}
+
+wordReverse("Now I know what a TV dinner feels like")
+wordReverse("Put Hans back on the line")
 
 // short1
 function maxofTwoNumbers (a, b) {
@@ -16,7 +26,7 @@ function maxofTwoNumbers (a, b) {
 maxofTwoNumbers(7, 5)
 
 // short2
-function maxofThree (a, b, c) {
+function maxofThree(a, b, c){
   if ((a > b) && (a > c)) {
     return a
   } else if (b > c) {
@@ -29,6 +39,22 @@ function maxofThree (a, b, c) {
 maxofThree(4, 1, 7)
 
 // short3
+var isVowel = false
+var vowels = ['a', 'e', 'i', 'o', 'u']
+
+function isCharacterAVowel (str) {
+  for (var i = 0; i < vowels.length; i++) {
+    console.log()
+    if () {
+      isVowel = true
+    } else {
+      isVowel = false
+    }
+  }
+  return isVowel
+}
+
+isCharacterAVowel( 's' )
 
 // short4
 
@@ -43,5 +69,18 @@ function reverseString (str) {
 reverseString('jag testar')
 
 // short7
+function findLongestWords (arr) {
+  var longestWordsLength = 0
+  for (var i = 0; i < arr.length; i++) {
+    arr[i]
+    arr[i].length
+    if ( longestWordsLength < arr[i].length ) {
+      longestWordsLength = arr[i].length
+    }
+  }
+  return longestWordsLength
+}
+
+findLongestWords()
 
 // short8
